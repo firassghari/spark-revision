6f297867e781ffa36c463da5014e0cc2baff8864
firas <firas.sghari@yahoo.fr>
2018-03-21 20:13:22 +0100

    filter example with dataframe

---
+++ filter-example.scala
@@ -0,0 +1,7 @@
+
+
+val salaries = sc.textFile("/spark/salaries/salaries.txt") 
+val salariesMap = salaries.map(row =>(row.split(",")(0).toInt, row.split(",")(1).toInt, row.split(",")(2).toInt));
+val salariesDF = salariesMap.toDF 
+val filtredDF = salariesDF.select("*").where("_2>2000")
+val rddMap = df.map(row=>(row(0) +"\t"+row(1)+"\t"+row(2)+"\t"))
