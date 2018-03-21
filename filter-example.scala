7778f0a26f64bfed33f23307bc5b9bd6c2085aa6
firas <firas.sghari@yahoo.fr>
2018-03-21 20:43:01 +0100

    rdd example

--- filter-example.scala
+++ filter-example.scala
@@ -1,7 +1,14 @@
 
-
+// dataframe 
 val salaries = sc.textFile("/spark/salaries/salaries.txt") 
 val salariesMap = salaries.map(row =>(row.split(",")(0).toInt, row.split(",")(1).toInt, row.split(",")(2).toInt));
 val salariesDF = salariesMap.toDF 
 val filtredDF = salariesDF.select("*").where("_2>2000")
 val rddMap = df.map(row=>(row(0) +"\t"+row(1)+"\t"+row(2)+"\t"))
+
+//RDD
+val salaries = sc.textFile("/spark/salaries/salaries.txt") 
+val salariesMap = salaries.map(row =>(row.split(",")(0).toInt, row.split(",")(1).toInt, row.split(",")(2).toInt));
+val filtredRDD = salariesMap.filter(_._2>2000)
+filtredRDD.foreach(println)
+
